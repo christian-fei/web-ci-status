96893d6e966ebeb8091bf42deee55f222aec795f
Christian Fei <crifei93@gmail.com>
2015-02-28 16:48:17 +0100

    comm to webhook-publisher

--- index.js
+++ index.js
@@ -7,14 +7,12 @@ var express = require('express')
   , bunyanFormat = require('bunyan-format')  
   , log = bunyan.createLogger({name: 'WEB_CI_STATUS', stream: bunyanFormat({ outputMode: 'short' })})
 
-log.info('test')
-
 var WEB_CI_STATUS_HTTP_PORT = process.env.WEB_CI_STATUS_HTTP_PORT || 3003
 var WEBHOOK_PUBLISHER_TCP_COMM_HOST = process.env.WEBHOOK_PUBLISHER_TCP_COMM_HOST || '146.185.167.197'
 var WEBHOOK_PUBLISHER_TCP_COMM_PORT = process.env.WEBHOOK_PUBLISHER_TCP_COMM_PORT || 3002
 
 var app = express()
-var tcpSocket = new net.Socket()
+var tcpSocket = new JSONSocket(new net.Socket())
 
 app.use(bodyParser.json())
 app.use(morgan('combined'))
@@ -29,17 +27,12 @@ var httpServer = app.listen(WEB_CI_STATUS_HTTP_PORT, function(){
 })
 
 tcpSocket.connect(WEBHOOK_PUBLISHER_TCP_COMM_PORT, WEBHOOK_PUBLISHER_TCP_COMM_HOST, function(){
-  log.info('TCP server listening on http://localhost:' + WEBHOOK_PUBLISHER_TCP_COMM_PORT)
+  log.info('TCP COMM client connected to http://localhost:%s', WEBHOOK_PUBLISHER_TCP_COMM_PORT)
 })
-.on('error', function(){
-  log.error('TCP server failed to connect')
+tcpSocket.on('error', function(){
+  log.error('TCP client failed to connect')
   process.exit(1)
 })
-.on('connection',function(socket){
-  var name = socket.remoteAddress + ':' + socket.remotePort + ':' + socket._handle.fd
-  socket = new JSONSocket(socket)
-  socket.name = name
-  socket.on('message', function(){
-    log.info('test')
-  })
+tcpSocket.on('message', function(data){
+  log.info(data)
 })
